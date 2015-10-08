921e85c26f5b76044cc840744e40805549624f71
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-10-08 16:01:03 -0700

    homework

--- word_frequency.py
+++ word_frequency.py
@@ -1,20 +1,28 @@
+import re
+
+
 def word_frequency(words, used):
     """takes in (key, value) and spits out a dictionary"""
     return dict(zip((words_used),(times_used)))
 
-with open("sample.txt") as file:
-    text = file.read().split()
+def space_remover(phrase):
+    """ Removes the punctuation from the string"""
+    return re.sub(r'[^A-Za-z ]', "", phrase)
 
-re.sub(r'[^A-Za-z]',)
+with open("sample.txt") as file:
+    text = file.read()
+    text = space_remover(text)
+    text = text.split()
 
 words_used = []
+# putting each word into a list
 for words in text:
     words_used.append(words)
 
 times_used = []
+# make a list of times each word was used
 for words in words_used:
     times_used.append(words_used.count(words))
 word_frequency_dict = word_frequency(words_used, times_used)
-
-
-print(word_frequency_dict)
+order = sorted(word_frequency_dict.items(), key=lambda x: x[1], reverse = True)
+print(order[:20])
